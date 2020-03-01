c984639d64ef20be1282f18c07ccedfba4c8ff39
Juho <juhovain@gmail.com>
2020-03-01 19:36:53 +0200

    Expanded posts query

--- server.js
+++ server.js
@@ -29,19 +29,15 @@ function getFeaturedImage(post_id, meta_key){
 
     return new Promise((resolve, reject) => {
 
-        connection.query('SELECT meta_value FROM wp_postmeta WHERE post_id="' + post_id + '" AND meta_key="_thumbnail_id"', (error, results, fields) => {
+        let query = 'SELECT meta_value FROM wp_postmeta WHERE meta_key="_wp_attachment_metadata" AND post_id=(SELECT meta_value FROM wp_postmeta WHERE post_id="'+post_id+'" AND meta_key="_thumbnail_id")';
+
+        connection.query(query, (error, results, fields) => {
 
             if(error) console.log(error);
 
             let value = results[0].meta_value;
 
-            connection.query('SELECT meta_value FROM wp_postmeta WHERE post_id="'+ value +'" AND meta_key="_wp_attachment_metadata"', (error, results, fields) => {
-
-                if(error) console.log(error);
-
-                resolve(unserialize(results[0].meta_value));
-
-            });
+            resolve(unserialize(value));
             
     
         });
@@ -53,9 +49,23 @@ function getFeaturedImage(post_id, meta_key){
 
 app.get('/posts', (req, res) => {
 
-    connection.query('SELECT ID, post_date, post_title, post_name, post_author, post_excerpt FROM wp_posts WHERE post_type="post" AND post_status="publish"', (error, results, fields) => {
+    let authorQuery = 'SELECT postmeta.meta_value FROM wp_usermeta postmeta WHERE user_id=posts.post_author AND meta_key="nickname"';
+    let featuredImageQuery = 'SELECT postmeta.meta_value FROM wp_postmeta postmeta WHERE meta_key="_wp_attachment_metadata" AND post_id=(SELECT postmeta.meta_value FROM wp_postmeta postmeta WHERE post_id=posts.ID AND meta_key="_thumbnail_id")';
+
+    connection.query('SELECT ID, posts.post_date, posts.post_title, posts.post_name, posts.post_excerpt, ('+ featuredImageQuery +') as post_thumbnail, ('+ authorQuery +') as post_author FROM wp_posts posts WHERE post_type="post" AND post_status="publish"', (error, results, fields) => {
 
         if(error) console.log(error);
+
+        results.forEach((item,index) => {
+
+            let thumbnail = unserialize(item.post_thumbnail);
+
+            let f = thumbnail.file.split('/');
+            thumbnail['base_url'] = 'https://blog.hoods.fi/wp-content/uploads/' + f[0] + '/' + f[1] + '/';
+
+            results[index].post_thumbnail = thumbnail;
+
+        });
 
         res.end(JSON.stringify(results));
 
@@ -80,11 +90,11 @@ app.get('/posts/:slug', (req, res) => {
 app.get('/posts/:post_id/image', (req, res) => {
 
     getFeaturedImage(req.params.post_id).then(image => {
-        let f = image.file.split('/');
+/*         let f = image.file.split('/');
         let featuredImage = {
             url: 'https://blog.hoods.fi/wp-content/uploads/' + f[0] + '/' + f[1] + '/' + image.sizes.medium_large.file
-        }
-        res.end(JSON.stringify(featuredImage));
+        } */
+        res.end(JSON.stringify(image));
     })
 
 });
